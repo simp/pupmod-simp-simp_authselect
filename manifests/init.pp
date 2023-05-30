e2252f50a35fa6f97e0ad3ad4e8514f3daa34cd4
Rick Gardner <Rick@sicura.us>
2023-05-30 15:31:48 -0400

    attempting simplib for lookup of pam::authselect_sections

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,28 +12,27 @@
 class simp_authselect (
   String $custom_profile_name                            = 'simp',
   Enum['sssd','winbind', 'nis', 'minimal'] $base_profile = 'sssd',
-  Array[String] $authselect_section				 = simplib::lookup('pam::auth_sections', { 'default_value' => ['fingerprint', 'system', 'password', 'smartcard'] })
+  Optional[Array[String]] $authselect_sections				 = undef 
   Boolean $use_authselect                                = simplib::lookup('simp_options::authselect', { 'default_value' => false })
 ) {
   if $use_authselect {
     include 'pam'
-    # Check against pam::auth_sections default is ['fingerprint', 'system', 'password', 'smartcard']
+    $_authselect_sections = $authselect_sections ? {
+      undef => simplib::lookup('pam::auth_sections', { 'default_value' => ['fingerprint', 'password', 'smartcard', 'system'] }), 
+      default => $authselect_sections 
+    }
+    # Check against pam::auth_sections default is [ifingerprint', 'system', 'password', 'smartcard']
     # We need to iterate through this array and ONLY put the include in for the ones defined in that array
     # or the files we're referencing won't exist. I'm not entirely sure how to access pam::auth_sections variable
     # but if we can we need to so something like the following:
   
-    $contents = $authselect_section.reduce({}) |$memo, $section| {
+    $contents = $_authselect_sections.reduce({}) |$memo, $section| {
       $memo + {
         "${section}-auth" => {
           'content' => "include '${custom_profile_name}/${section}-auth'",
         },
       }
     }
-  #  $authselect_section.each  |String $auth_section| { 
-  #    $contents[$auth_section] = {
-  #      'content' => "include '${custom_profile_name}/${auth_section}-auth'"
-  #    }
-  #  }
   
     #instantiate the authselect class with the given authselect
     class { 'authselect':
